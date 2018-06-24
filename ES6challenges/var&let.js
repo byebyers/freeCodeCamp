1e0cacbc37e717551c500e550eb027eccca49084
Jacob Byers <byers.jacob@gmail.com>
2018-06-24 12:02:13 -0700

    Added example to use Object.freeze

--- ES6challenges/var&let.js
+++ ES6challenges/var&let.js
@@ -77,8 +77,35 @@ function editInPlace() {
   s[0] = 2;
   s[1] = 5;
   s[2] = 7;
-  
+
   // change code above this line
 }
 editInPlace();
+*/
+
+
+//Prevent Object Mutation
+
+/*
+Since const are not immune to changes we can use Object.freeze to ensure the data is not changed.
+*/
+
+/*
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+
+  Object.freeze(MATH_CONSTANTS);
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
 */
