5dcdf0a4cac71e8535fa5770827c5dd5af163bcb
Jacob Byers <byers.jacob@gmail.com>
2018-06-24 11:23:32 -0700

    Playing with let in block function

--- ES6challenges/var&let.js
+++ ES6challenges/var&let.js
@@ -1,7 +1,9 @@
 /*
 Changing var to let allows the variable to be only called only once.
-This helps when dealing with larger code when you may overite a varable accidently. 
+This helps when dealing with larger code when you may overite a varable accidently.
 */
+
+/*
 let catName;
 let quote;
 function catTalk() {
@@ -12,3 +14,22 @@ function catTalk() {
 
 }
 catTalk();
+*/
+
+//ES6: Compare Scopes of the var and let Keywords
+
+/*
+By declaring a let function in the block, we do not affect the varable outside.
+*/
+
+function checkScope() {
+  function checkScope() {
+  "use strict";
+    let i = "function scope";
+    if (true) {
+      let i = "block scope";
+      console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+  }
