d5ce05ff4a4c1682e451bc073868633ba7919697
Jacob Byers <byers.jacob@gmail.com>
2018-06-28 13:36:55 -0700

    Example of an arrow function using higher order functions.

--- ES6challenges/arrowFunctions.js
+++ ES6challenges/arrowFunctions.js
@@ -31,3 +31,20 @@ Like a normal function you can pass Parameters through an arrow function.
 const doubler = (item) => item * 2;
 
 */
+
+
+//Write Higher Order Arrow Functions
+
+/*
+Since Arrow Functions work well with higher order functions you cat turn.
+
+FBPosts.filter(function(post) {
+  return post.thumbnail !== null && post.shares > 100 && post.likes > 500;
+})
+
+to
+
+FBPosts.filter((post) => post.thumbnail !== null && post.shares > 100 && post.likes > 500)
+
+Simper simple
+*/
