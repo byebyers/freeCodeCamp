00f4d7d5ab96c249bef56bd9c5e706480979d241
Jacob Byers <byers.jacob@gmail.com>
2018-06-28 13:26:47 -0700

    Example to use an arrow function instead of an anonymous function

---
+++ ES6challenges/arrowFunctions.js
@@ -0,0 +1,23 @@
+//Use Arrow Functions to Write Concise Anonymous Functions
+
+/*
+We can make unnamed functions as inline functions like this.
+
+const myFunc = function() {
+  const myVar = "value";
+  return myVar;
+}
+
+With ES6 howerver we can achieve writing Anonymous functions with arrow functions like this.
+
+const myFunc = () => {
+  const myVar = "value";
+  return myVar;
+}
+
+Since that function above is only returning a vaue we can call the function like this instead.
+
+const myFunc= () => "value"
+
+It is simpler to do it this way.
+*/
