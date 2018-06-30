4614aca78733efe708c08f9625064d957556ebdc
Jacob Byers <byers.jacob@gmail.com>
2018-06-29 18:25:58 -0700

    Added an example of higher order functions

--- ES6challenges/arrowFunctions.js
+++ ES6challenges/arrowFunctions.js
@@ -48,3 +48,21 @@ FBPosts.filter((post) => post.thumbnail !== null && post.shares > 100 && post.li
 
 Simper simple
 */
+
+/*
+
+Use arrow function syntax to compute the square of only the positive integers (fractions are not integers) in the array realNumberArray and store the new array in the variable squaredIntegers.
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter(num => Number.isInteger(num)).map(square => square*square);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+*/
