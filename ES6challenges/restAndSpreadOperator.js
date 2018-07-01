e0f801dbf9519aca170317d91109863c9822dfe7
Jacob Byers <byers.jacob@gmail.com>
2018-07-01 14:35:58 -0700

    Added Spread operator example

--- ES6challenges/restAndSpreadOperator.js
+++ ES6challenges/restAndSpreadOperator.js
@@ -21,5 +21,39 @@ const sum = (function() {
 })();
 console.log(sum(1, 2, 3)); // 6
 
-The way you set up arguments is by using (...args)
+The way you set up arguments is by using (...argument)
+*/
+
+
+//Use the Spread Operator to Evaluate Arrays In-Place
+
+/*
+ES6 introduces the spread operator, which allows us to expand arrays and other expressions in places where multiple parameters or elements are expected.
+
+The ES5 code below uses apply() to compute the maximum value in an array:
+
+    var arr = [6, 89, 3, 45];
+    var maximus = Math.max.apply(null, arr); // returns 89
+
+We had to use Math.max.apply(null, arr) because Math.max(arr) returns NaN. Math.max() expects comma-separated arguments, but not an array.
+
+The spread operator makes this syntax much better to read and maintain.
+
+    const arr = [6, 89, 3, 45];
+    const maximus = Math.max(...arr); // returns 89
+
+
+...arr returns an unpacked array. In other words, it spreads the array.
+
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
+
+use ... for the spread
+
 */
