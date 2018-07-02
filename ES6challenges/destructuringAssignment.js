70b17875e2e65353132022996a1b673687d5abb8
Jacob Byers <byers.jacob@gmail.com>
2018-07-02 10:06:10 -0700

    Destructuring Assignment to Assign Variables from Objects

---
+++ ES6challenges/destructuringAssignment.js
@@ -0,0 +1,39 @@
+//Use Destructuring Assignment to Assign Variables from Objects
+
+/*
+We can do something similar with objects as well. Destructuring assignment is special syntax for neatly assigning values taken directly from an object to variables.
+
+Consider the following ES5 code:
+
+    var voxel = {x: 3.6, y: 7.4, z: 6.54 };
+    var x = voxel.x; // x = 3.6
+    var y = voxel.y; // y = 7.4
+    var z = voxel.z; // z = 6.54
+
+Here's the same assignment statement with ES6 destructuring syntax:
+
+    const { x, y, z } = voxel; // x = 3.6, y = 7.4, z = 6.54
+
+If instead you want to store the values of voxel.x into a, voxel.y into b, and voxel.z into c, you have that freedom as well.
+
+    const { x : a, y : b, z : c } = voxel // a = 3.6, b = 7.4, c = 6.54
+
+
+Here is some example code.
+
+function getLength(str) {
+  "use strict";
+
+  // change code below this line
+  const {length : len} = str; // change this
+  // change code above this line
+
+  return len; // you must assign length to len in line
+
+}
+
+console.log(getLength('FreeCodeCamp'));
+
+Essentially we are assigning .length to len then calling it back for a response. Easier way to
+write it. 
+*/
