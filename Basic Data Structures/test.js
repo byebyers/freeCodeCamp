953c69df8fbe6144aeafb0ed44b9c355088f2f0b
Jacob Byers <byers.jacob@gmail.com>
2018-08-27 16:14:39 -0700

    Example of accessing property names with bracket notation.

--- Basic Data Structures/test.js
+++ Basic Data Structures/test.js
@@ -1,18 +1,18 @@
-let nestedArray = [ // top, or first level - the outer most array
-  ['deep'], // an array within an array, 2 levels of depth
-  [
-    ['deeper'], ['deeper'] // 2 arrays nested 3 levels deep
-  ],
-  [
-    [
-      ['deepest'], ['deepest'] // 2 arrays nested 4 levels deep
-    ],
-    [
-      [
-        ['deepest-est?'] // an array nested 5 levels deep
-      ]
-    ]
-  ]
-];
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+// do not change code above this line
 
-console.log(nestedArray[2][1][0][0][0]);
+//console.log(foods.apples);
+
+
+function checkInventory(scannedItem) {
+  console.log(foods.scannedItem);
+}
+
+checkInventory("apples");
