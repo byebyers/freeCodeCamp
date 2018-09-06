4699005c8446447b3a7bbeb6c5a48a7e90da4b09
Jacob Byers <byers.jacob@gmail.com>
2018-09-06 16:37:51 -0700

    Example to find the longest word.

---
+++ Basic Algorithm Scripting/findTheLongestWordInAString.js
@@ -0,0 +1,26 @@
+//Find the Longest Word in a String
+
+/*
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+*/
+
+/*
+function findLongestWordLength(str) {
+  const stringArray = str.split(" ");
+  const orderedArray = stringArray.sort((a, b) => {
+    return a.length < b.length;
+  });
+
+  return orderedArray[0].length;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+*/
+
+/*
+
+*/
--- Basic Algorithm Scripting/test.js
+++ Basic Algorithm Scripting/test.js
@@ -1,11 +1,13 @@
-function reverseString(str) {
-  let splitStr = str.split("");
-  console.log(splitStr);
-  let revStr = splitStr.reverse();
-  console.log(revStr);
-  let newStr = revStr.join('');
-  console.log(newStr);
-  return newStr;
+function largestOfFour(arr) {
+  // You can do this!
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }
+  return arr;
 }
 
-reverseString("hello");
+
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
