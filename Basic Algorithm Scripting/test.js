0af6ede7d526772f6ec9711b3df9f0a1f473c1f4
Jacob Byers <byers.jacob@gmail.com>
2018-09-08 16:31:52 -0700

    Examples of scripting with splice and slice.

--- Basic Algorithm Scripting/test.js
+++ Basic Algorithm Scripting/test.js
@@ -1,21 +1,12 @@
-function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let copy = arr1.slice();
+  console.log(copy);
+
+  arr2.splice(n,0,...copy);
+  console.log(arr2);
 }
 
-function titleCase(str) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
-}
-
-titleCase("I'm a little tea pot");
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 //return str.substr(-target.length) === target ? true : false;
