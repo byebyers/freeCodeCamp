7571fe34a18f448258152c9f9f0193a00bf304a8
Jacob Byers <byers.jacob@gmail.com>
2018-09-11 13:31:08 -0700

    Added another example to solve mutations problem.

--- Basic Algorithm Scripting/mutations.js
+++ Basic Algorithm Scripting/mutations.js
@@ -25,6 +25,20 @@ function mutation(arr) {
 mutation(["hello", "hey"]);
 */
 
+
+//Another way to solve the problem with the .sort method
+
 /*
+function mutation(arr) {
+  arr.sort(function(a, b){
+    return a.length - b.length;
+  });
+  return arr[0].toLowerCase().split("").every(function(x){
+    if(arr[1].toLowerCase().indexOf(x) === -1){
+      return false;
+    }
+    else true;
+  });
+}
 
 */
