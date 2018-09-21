3abdaa1d754e8dfd887b8173f960730660f766c6
Jacob Byers <byers.jacob@gmail.com>
2018-09-21 16:55:24 -0700

    Example of refactoring Global Variables out of functions.

---
+++ Functional Programming/test.js
@@ -0,0 +1,38 @@
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (testList, bookName) {
+
+  let newList = [];
+  console.log(newList);
+  newList.push(testList);
+
+  return newList.push(bookName);
+
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (testList, bookName) {
+  let myList = [];
+  myList.push(testList);
+  if (myList.indexOf(bookName) >= 0) {
+
+    return myList.splice(0, 1, bookName);
+
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(newBookList);
