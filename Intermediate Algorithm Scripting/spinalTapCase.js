d19ea014796cb34f7a6fc6f877bf9af62961d5af
Jacob Byers <byers.jacob@gmail.com>
2018-10-02 13:49:35 -0700

    Added an additional solution to Spinal Tap Case.

--- Intermediate Algorithm Scripting/spinalTapCase.js
+++ Intermediate Algorithm Scripting/spinalTapCase.js
@@ -21,5 +21,16 @@ spinalCase('This Is Spinal Tap');
 */
 
 /*
+Another way to solve This
 
+function spinalCase(str) {
+  // Create a variable for the white space and underscores.
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  // Replace space and underscore with -
+  return str.replace(regex, '-').toLowerCase();
+}
 */
