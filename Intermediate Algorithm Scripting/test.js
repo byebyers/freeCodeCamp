fd762577a692bf2d69f3a828b03194b2966e8c1c
Jacob Byers <byers.jacob@gmail.com>
2018-10-03 16:09:26 -0700

    Solution to pig latin

--- Intermediate Algorithm Scripting/test.js
+++ Intermediate Algorithm Scripting/test.js
@@ -1,11 +1,32 @@
-function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  let fixStr = str.replace(/([A-Z])/g, ' $1');
-  let lowerStr = fixStr.toLowerCase();
-  let splitStr = lowerStr.split(/\W+|[_]/).filter(x => x.length > 1);
-  let joinStr = splitStr.join("-");
-  console.log(joinStr)
+function translatePigLatin(str) {
+  let splitStr = str.split("");
+  let vowelStr = [];
+  let newStr = [];
+  let conStr = [];
+  if (splitStr[0] == "a" || splitStr[0] == "e" || splitStr[0] == "i" || splitStr[0] == "o" || splitStr[0] == "u") {
+    let vowelStr = splitStr;
+    let vowel = ['w', 'a', 'y'];
+    vowelStr = vowelStr.concat(vowel);
+    vowelStr = vowelStr.join("");
+    console.log(vowelStr);
+  }
+  else if (splitStr[1] == "l") {
+    let conStr = splitStr.splice(2);
+    let conExtra = ['a', 'y'];
+    conStr = conStr.concat(splitStr);
+    conStr = conStr.concat(conExtra);
+    conStr = conStr.join("");
+    console.log(conStr);
+  }
+  else {
+    let newStr = splitStr.splice(1);
+    let extra = ['a', 'y'];
+    newStr = newStr.concat(splitStr);
+    newStr = newStr.concat(extra);
+    newStr = newStr.join("");
+    console.log(newStr);
+  }
+  console.log(splitStr);
 }
 
-spinalCase('The_Andy_Griffith_Show');
+translatePigLatin("glove");
