dac0e8e03ca50f0b8059dee99cd147f0cc691ada
Jacob Byers <byers.jacob@gmail.com>
2018-10-04 12:05:40 -0700

    Corrected Pig Latin file type.

--- Intermediate Algorithm Scripting/test.js
+++ Intermediate Algorithm Scripting/test.js
@@ -1,32 +1,8 @@
-function translatePigLatin(str) {
-  let splitStr = str.split("");
-  let vowelStr = [];
-  let newStr = [];
-  let conStr = [];
-  if (splitStr[0] == "a" || splitStr[0] == "e" || splitStr[0] == "i" || splitStr[0] == "o" || splitStr[0] == "u") {
-    let vowelStr = splitStr;
-    let vowel = ['w', 'a', 'y'];
-    vowelStr = vowelStr.concat(vowel);
-    vowelStr = vowelStr.join("");
-    console.log(vowelStr);
-  }
-  else if (splitStr[1] == "l") {
-    let conStr = splitStr.splice(2);
-    let conExtra = ['a', 'y'];
-    conStr = conStr.concat(splitStr);
-    conStr = conStr.concat(conExtra);
-    conStr = conStr.join("");
-    console.log(conStr);
-  }
-  else {
-    let newStr = splitStr.splice(1);
-    let extra = ['a', 'y'];
-    newStr = newStr.concat(splitStr);
-    newStr = newStr.concat(extra);
-    newStr = newStr.join("");
-    console.log(newStr);
-  }
-  console.log(splitStr);
+function diffArray(arr1, arr2) {
+  let difference = arr1
+                  .filter(x => !arr2.includes(x))
+                  .concat(arr2.filter(x => !arr1.includes(x)))
+  console.log(difference);
 }
 
-translatePigLatin("glove");
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5, 6]);
