2e7a900cdb4c6650f17f8b6248db67a9107a3309
Jacob Byers <byers.jacob@gmail.com>
2018-10-08 13:19:57 -0700

    Solution to Sum of all primes.

--- Intermediate Algorithm Scripting/test.js
+++ Intermediate Algorithm Scripting/test.js
@@ -1,24 +1,33 @@
-function printPrime(value) {
-    var primes = [];
-    for(var i = 2; i < value; i++) {
-        primes[i] = true;
-    }
-    var limit = Math.sqrt(value);
-    console.log(limit);
-    for(var i = 2; i < limit; i++) {
-        console.log(i);
-        if(primes[i] === true) {
-            for(var j = i * i; j < value; j += i) {
-                console.log(j);
-                primes[j] = false;
-            }
-        }
-    }
-    for(var i = 2; i < value; i++) {
-        if(primes[i] === true) {
-            console.log(i + " " + primes[i]);
-        }
-    }
+function sumPrimes(num) {
+  // function to check if the number presented is prime
+  function isPrime(number){
+      for (let i = 2; i <= number; i++){
+          if(number % i === 0 && number!= i){
+          // return true if it is divisible by any number that is not itself.
+             return false;
+          }
+       }
+       // if it passes the for loops conditions it is a prime
+      return true;
+  }
+  // 1 is not a prime, so return nothing, also stops the recursive calls.
+  if (num === 1){
+    return 0;
+  }
+  // Check if your number is not prime
+  if(isPrime(num) === false){
+  // for non primes check the next number down from your maximum number, do not add anything to your answer
+    return sumPrimes(num - 1);
+  }
+
+  // Check if your number is prime
+  if(isPrime(num) === true){
+  // for primes add that number to the next number in the sequence through a recursive call to our sumPrimes function.
+
+    return num + sumPrimes(num - 1);
+  }
 }
 
-printPrime(10);
+
+console.log(sumPrimes(10));
+sumPrimes(10);
