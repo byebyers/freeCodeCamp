2dbe45b1fbcc4f679bcc91a5375c080eddddbfd6
Jacob Byers <byers.jacob@gmail.com>
2018-10-09 13:25:24 -0700

    Added another solution to Missing Letters

--- Intermediate Algorithm Scripting/missingLetters.js
+++ Intermediate Algorithm Scripting/missingLetters.js
@@ -109,3 +109,23 @@ function fearNotLetter(str) {
 
 fearNotLetter("stvwx");
 */
+
+/*
+Another way to solve This
+
+function uniteUnique(arr) {
+  let args = Array.from(arguments);
+  let newArr = [];
+  for (let i = 0; i < args.length; i++) {
+    for(let j = 0; j < args[i].length; j++) {
+      if (newArr.indexOf(args[i][j]) === -1) {
+      newArr.push(args[i][j]);
+      }
+    }
+  }
+  return newArr;
+}
+
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+*/
