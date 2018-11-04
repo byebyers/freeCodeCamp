b74850ff27e9bc0ab5380e870cf95efe396738dc
Jacob Byers <byers.jacob@gmail.com>
2018-11-04 15:47:48 -0700

    Solution to Arguments Optional

--- Intermediate Algorithm Scripting/test.js
+++ Intermediate Algorithm Scripting/test.js
@@ -1,15 +1,54 @@
-function truthCheck(collection, pre) {
-  // Is everyone being true?
-  var size = collection.length;
-  var result = true;
-  for (let i = 0; i < size; i++) {
-    let key = Object.keys(collection[i]);
-    let test = key.every(x => key.includes(pre));
-    if (test === false) {
-      result = false;
-    }
-  }
-  return result;
+/*
+function addTogether(x) {
+  return function(y) {
+      if (y) {
+        console.log(x+y);
+      }
+      console.log(x);
+    };
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+addTogether(2,3);
+
+
+function add(x) {
+  // Add your code below this line
+  return function(y) {
+    return function (z) {
+      return x + y + z;
+    }
+  }
+
+
+  // Add your code above this line
+}
+add(10)(20)(30);
+*/
+
+
+
+
+
+
+function addTogether(first, second) {
+  if (first === parseInt(first)) {
+    if (!second) {
+      return function(y) {
+        return first + y;
+      }
+    }
+    else {
+      if (second === parseInt(second)) {
+        return first + second;
+      }
+      else {
+        return undefined;
+      }
+    }
+  }
+  else {
+    return undefined;
+  }
+}
+
+addTogether(2,"3");
