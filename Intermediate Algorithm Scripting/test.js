a26b5fbba8ad797a1870ee4b11416e310cc826d8
Jacob Byers <byers.jacob@gmail.com>
2018-11-07 16:01:45 -0700

    Solution to make a person.

--- Intermediate Algorithm Scripting/test.js
+++ Intermediate Algorithm Scripting/test.js
@@ -1,54 +1,46 @@
+var Person = function(firstAndLast) {
+  // Complete the method below and implement the others similarly
+  var splitName = firstAndLast.split(" ");
+  var first = splitName[0];
+  var last = splitName[1];
+  var newName = first + " " + last;
+  console.log(newName);
+  this.setFirstName = function (firstName) {
+    first = firstName;
+    return first;
+  };
+  this.setLastName = function (lastName) {
+    last = lastName;
+    console.log(last);
+  };
+  this.setFullName = function (newFirstAndLast) {
+    var newSplit = newFirstAndLast.split(" ");
+    first = newSplit[0];
+    last = newSplit[1];
+    return newName;
+  };
+  this.getFirstName = function() {
+    return first;
+  };
+  this.getLastName = function() {
+    return last;
+  };
+  this.getFullName = function() {
+    return newName;
+  };
+  return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.setLastName("Haskell")
+bob.getLastName();
+console.log(bob.getLastName);
+
 /*
-function addTogether(x) {
-  return function(y) {
-      if (y) {
-        console.log(x+y);
-      }
-      console.log(x);
-    };
-}
-
-addTogether(2,3);
-
-
-function add(x) {
-  // Add your code below this line
-  return function(y) {
-    return function (z) {
-      return x + y + z;
-    }
-  }
-
-
-  // Add your code above this line
-}
-add(10)(20)(30);
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
 */
-
-
-
-
-
-
-function addTogether(first, second) {
-  if (first === parseInt(first)) {
-    if (!second) {
-      return function(y) {
-        return first + y;
-      }
-    }
-    else {
-      if (second === parseInt(second)) {
-        return first + second;
-      }
-      else {
-        return undefined;
-      }
-    }
-  }
-  else {
-    return undefined;
-  }
-}
-
-addTogether(2,"3");
