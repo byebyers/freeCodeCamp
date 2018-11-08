d11b797d1d7ba02184ab98017a498229fc245a7e
Jacob Byers <byers.jacob@gmail.com>
2018-11-08 14:18:50 -0700

    Solution to Map the Debris

--- Intermediate Algorithm Scripting/test.js
+++ Intermediate Algorithm Scripting/test.js
@@ -1,40 +1,29 @@
+
 var Person = function(firstAndLast) {
   // Complete the method below and implement the others similarly
-  var splitName = firstAndLast.split(" ");
-  var first = splitName[0];
-  var last = splitName[1];
-  var newName = first + " " + last;
-  console.log(newName);
-  this.setFirstName = function (firstName) {
-    first = firstName;
-    return first;
-  };
-  this.setLastName = function (lastName) {
-    last = lastName;
-    console.log(last);
-  };
-  this.setFullName = function (newFirstAndLast) {
-    var newSplit = newFirstAndLast.split(" ");
-    first = newSplit[0];
-    last = newSplit[1];
-    return newName;
-  };
-  this.getFirstName = function() {
-    return first;
-  };
-  this.getLastName = function() {
-    return last;
-  };
-  this.getFullName = function() {
-    return newName;
-  };
-  return firstAndLast;
-};
 
-var bob = new Person('Bob Ross');
-bob.setLastName("Haskell")
-bob.getLastName();
-console.log(bob.getLastName);
+  var name = firstAndLast.split(' ');
+
+  this.getFirstName = function() {
+    return name[0];
+  }
+  this.getLastName = function() {
+    return name[1];
+  }
+  this.getFullName = function() {
+    return name.join(' ');
+  };
+  this.setFirstName = function(first) {
+    name[0] = first;
+  }
+  this.setLastName = function(last) {
+    name[1] = last;
+  }
+  this.setFullName = function(firstAndLast) {
+    name = firstAndLast.split(' ');
+  }
+
+};
 
 /*
 getFirstName()
